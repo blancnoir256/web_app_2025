5bf2fe6f627af7ae22416ae975f21a265ef685dd
bn256 <blancnoir256@gmail.com>
2025-06-16 17:35:21 +0900

    TodoListコンポーネントのコードを整理し、変数名を修正。表示タイトルを修正し、スタイルを整えました。

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,46 +7,64 @@ interface Todo {
 }
 
 const todos = ref<Todo[]>([
-    { name: '生命実験のレポートを書く', isComplete:true },
-    { name: '全強になる', isComplete:false }
+    { name: '生命実験のレポートを書く', isComplete: true },
+    { name: '全強になる', isComplete: false }
 ])
 const newTodoName = ref('')
-const newItemIsComplete = ref(false)
+const newTodoIsComplete = ref(false)
 
-const addItem = () => {
-    if (newTodoName.value == '') {
+const addtodo = () => {
+    if (newTodoName.value === '') {
         return
     }
-    todos.value.push({ name: newTodoName.value, isComplete: newItemIsComplete.value })
+    todos.value.push({ name: newTodoName.value, isComplete: newTodoIsComplete.value })
     newTodoName.value = ''
-    newItemIsComplete.value = false
+    newTodoIsComplete.value = false
 }
 </script>
 
 <template>
     <div>
-        <h2 class="medatsu">ItemList</h2>
+        <h2 class="medatsu">TodoList</h2>
+        <h3>completed list</h3>
         <ul>
-            <li v-for="item in todos" :key="item.name">
-                <div :class="{ completed: item.isComplete }">やること: {{ item.name }}</div>
+            <li v-for="todo in todos.filter(todo => todo.isComplete)" :key="todo.name">
+                <div :class="{ completed: todo.isComplete }">やること: {{ todo.name }}
+                    <input type="checkbox" v-model="todo.isComplete">
+                </div>
             </li>
+            <div v-if="todos.filter(todo => todo.isComplete).length == 0">
+                特になし
+            </div>
+        </ul>
+        <h3>uncompleted list</h3>
+        <ul>
+            <li v-for="todo in todos.filter(todo => !todo.isComplete)" :key="todo.name">
+                <div :class="{ completed: todo.isComplete }">やること: {{ todo.name }}
+                    <input type="checkbox" v-model="todo.isComplete">
+                </div>
+            </li>
+            <div v-if="todos.filter(todo => !todo.isComplete).length == 0">
+                特になし
+            </div>
         </ul>
         <div>
             <label>
                 Todo
                 <input v-model="newTodoName" type="text" />
             </label>
-            <button @click="addItem">add</button>
+            <button @click="addtodo">add</button>
         </div>
     </div>
 </template>
 
 <style>
 .completed {
-    text-decoration:line-through;
+    text-decoration: line-through;
     color: gray;
 }
+
 .medatsu {
-    color:yellowgreen
+    color: yellowgreen
 }
 </style>
